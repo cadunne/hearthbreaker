80fb857c255fc0a4a30b9680db837ed65600d1ca
cadunne <dunne064@umn.edu>
2015-05-10 22:20:12 -0500

    Revert 55ee854..268f2a7
    This rolls back to commit 55ee854405129c935cc1fa542128420b423361d7.

--- run_games.py
+++ run_games.py
@@ -1,5 +1,9 @@
 import json
+import logging
 from hearthbreaker.agents.basic_agents import RandomAgent
+from hearthbreaker.agents.aggressive_agent import AggressiveAgent
+from hearthbreaker.agents.control_agent import ControlAgent
+from hearthbreaker.agents.tempo_agent import TempoAgent
 from hearthbreaker.cards.heroes import hero_for_class
 from hearthbreaker.constants import CHARACTER_CLASS
 from hearthbreaker.engine import Game, Deck, card_lookup
@@ -29,17 +33,26 @@ def load_deck(filename):
     return Deck(cards, hero_for_class(character_class))
 
 
-def do_stuff():
+def do_stuff(agent1, agent2, d1, d2):
     _count = 0
+    lwins = 0
+    rwins = 0
+    
 
     def play_game():
         nonlocal _count
+        nonlocal lwins
+        nonlocal rwins
         _count += 1
+        
         new_game = game.copy()
         try:
+
             new_game.start()
-            print("game_done")
-            print(new_game.players[0].hero.health, new_game.players[1].hero.health, "####")
+
+            lwins += (new_game.players[0].hero.health != 0)
+            rwins += (new_game.players[1].hero.health != 0)
+
         except Exception as e:
             print(json.dumps(new_game.__to_json__(), default=lambda o: o.__to_json__()))
             print(new_game._all_cards_played)
@@ -47,14 +60,78 @@ def do_stuff():
 
         del new_game
 
-        if _count % 1000 == 0:
+        if _count % 50 == 0:
             print("---- game #{} ----".format(_count))
+            print("lwins: " + str(lwins) + ", rwins: "+ str(rwins))
 
-    deck1 = load_deck("example.hsdeck")
-    deck2 = load_deck("example.hsdeck")
-    game = Game([deck1, deck2], [RandomAgent(), RandomAgent()])
 
-    print(timeit.timeit(play_game, 'gc.enable()', number=1))
+    
+    deck1 = load_deck(d1)
+    deck2 = load_deck(d2)
+    game = Game([deck1, deck2], [agent1, agent2])
+
+    print(timeit.timeit(play_game, 'gc.enable()', number=5))
+
+    returnString = str(type(game.players[0].agent)) + " with deck " + d1 + ": " + str(lwins) + " wins"
+    returnString += '\n\t' + str(type(game.players[1].agent)) + " with deck " + d2 + ": " + str(rwins) + " wins"
+    if rwins != 0:
+        returnString += '\n\t ratio: ' + str(lwins/rwins)
+    
+    return returnString
 
 if __name__ == "__main__":
-    do_stuff()
+    rd = "rampDruid.hsdeck"
+    vm = "valueMage.hsdeck"
+    bh = "beastHunter.hsdeck"
+
+
+    logstring = do_stuff(TempoAgent(), ControlAgent(), rd, rd)
+
+
+    ra = RandomAgent()
+    ta = TempoAgent()
+    ca = ControlAgent()
+    aa = AggressiveAgent()
+
+    agentList = [ra, ta, ca, aa]
+
+    LOG_FILENAME = 'match_results.log'
+    logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG, filemode = 'w')
+
+    for x in range(0, 4):
+
+        ra = RandomAgent()
+        ta = TempoAgent()
+        ca = ControlAgent()
+        aa = AggressiveAgent()
+
+        agentList = [ra, ta, ca, aa]
+
+        a1 = agentList[x]
+        a2 = agentList[0]
+
+        logstring = do_stuff(a1, a2, rd, rd)
+        logging.debug(logstring)
+
+        logstring = do_stuff(a1, a2, rd, vm)
+        logging.debug(logstring)
+
+        logstring = do_stuff(a1, a2, vm, rd)
+        logging.debug(logstring)
+
+        logstring = do_stuff(a1, a2, vm, vm)
+        logging.debug(logstring)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
